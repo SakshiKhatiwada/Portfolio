cb48cfa9d583197f7fcc4a8c81de9210db0fa661
Sakshi Khatiwada <sakshikhatiwada37@gmail.com>
2025-03-06 10:12:38 +0545

    reviewed

---
+++ portfolio/src/components/Skills.jsx
@@ -0,0 +1,12 @@
+import './skills.css'
+const Skills = () => {
+  return (
+    <section className="skills">
+            <div className="progress-bar">
+                50%
+            </div>
+    </section>
+  )
+}
+
+export default Skills
